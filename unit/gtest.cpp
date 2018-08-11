eceae6b77a70b4832299cf3a66a4f86b738f4cc3
Tai-Min <22417805+Tai-Min@users.noreply.github.com>
2018-08-11 13:37:12 +0200

    code cleanup

--- unit/gtest.cpp
+++ unit/gtest.cpp
@@ -23,22 +23,27 @@ void loop()
 
 BoostSerial s;
 
+//check write() for one byte
 TEST(Serial, write)
 {
+    //write one byte and check whether arduino responded with the same byte
     ASSERT_EQ(1, s.write(10));
     while (!s.available())
         ;
     ASSERT_EQ(10, s.read());
 }
 
+//check whether write() works with rvalues
 TEST(Serial, writeBytestRval)
 {
+    //write some bytes and check whether arduino responded with the same bytes
     ASSERT_EQ(5, s.write({1, 2, 5, 7, 19}));
     while (!s.available())
         ;
     ASSERT_EQ(std::vector<uint8_t>({1, 2, 5, 7, 19}), s.readBytes());
 }
 
+//check whether write() works with lvals
 TEST(Serial, writeBytesLval)
 {
     std::vector<uint8_t> v({1, 6, 10, 245});
@@ -48,6 +53,7 @@ TEST(Serial, writeBytesLval)
     ASSERT_EQ(std::vector<uint8_t>({1, 6, 10, 245}), s.readBytes());
 }
 
+//check print() for various data types
 TEST(Serial, printTemplate)
 {
     ASSERT_EQ(2, s.print((int)11));
@@ -94,10 +100,7 @@ TEST(Serial, printTemplate)
     while (!s.available())
         ;
     ASSERT_EQ("10100110101", s.readString());
-}
 
-TEST(Serial, printString)
-{
     std::string t = "string1";
     ASSERT_EQ(7, s.print(t));
     while (!s.available())
@@ -111,16 +114,14 @@ TEST(Serial, printString)
     ASSERT_EQ("string1", s.readString());
 }
 
+//its print() with newline added, so nothing to test tbh
 TEST(Serial, printlnTemplate)
 {
     ASSERT_EQ(5, s.println("Test"));
     while (!s.available())
         ;
     ASSERT_EQ("Test\n", s.readString());
-}
 
-TEST(Serial, printlnString)
-{
     std::string t = "Min";
     ASSERT_EQ(4, s.println(t));
     while (!s.available())
@@ -130,6 +131,7 @@ TEST(Serial, printlnString)
 
 TEST(Serial, read)
 {
+    //should return -1 on empty buffer
     ASSERT_EQ(-1, s.read());
 
     ASSERT_EQ(1, s.write(20));
@@ -145,6 +147,8 @@ TEST(Serial, readBytes)
         ;
     ASSERT_EQ(std::vector<uint8_t>({10, 20, 30, 44}), s.readBytes());
 
+    //this should be not equal
+    //because readBytes can't process it in given timeout
     s.setTimeout(2);
     ASSERT_EQ(36, s.write(std::vector<uint8_t>({10, 20, 30, 44, 2, 2, 4, 5, 6, 2, 3, 5, 6, 76, 2, 234, 5, 6, 7, 23, 3, 2, 3, 3, 3, 3, 1, 2, 2, 3, 3, 34, 3, 1, 1, 1})));
     while (!s.available())
@@ -165,6 +169,8 @@ TEST(Serial, readBytesUntil)
     ASSERT_EQ(std::vector<uint8_t>({1, 2, 4}), s.readBytesUntil(7));
     s.flush();
 
+    //this should be not equal
+    //because readBytes can't process it in given timeout
     s.setTimeout(2);
     ASSERT_EQ(37, s.write(std::vector<uint8_t>({10, 20, 30, 44, 2, 2, 4, 5, 6, 2, 3, 5, 6, 76, 2, 234, 5, 6, 7, 23, 3, 2, 3, 3, 3, 3, 1, 2, 2, 3, 3, 34, 3, 1, 1, 1, 255})));
     while (!s.available())
@@ -184,6 +190,8 @@ TEST(Serial, readString)
         ;
     ASSERT_EQ("Hello", s.readString());
 
+    //this should be not equal
+    //because readBytes can't process it in given timeout
     s.setTimeout(2);
     ASSERT_EQ(79, s.print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"));
     while (!s.available())
@@ -206,6 +214,8 @@ TEST(Serial, readStringUntil)
         ;
     s.flush();
 
+    //this should be not equal
+    //because readBytes can't process it in given timeout
     s.setTimeout(2);
     ASSERT_EQ(92, s.print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaaaAAAAAAAAAAAABAAAAAAAAAA"));
     while (!s.available())
@@ -233,6 +243,7 @@ TEST(Serial, peek)
     s.flush();
 }
 
+//check how this class behaves on big data
 TEST(Serial, writeALot)
 {
     s.setTimeout(1000);
@@ -251,6 +262,8 @@ int main(int argc, char **argv)
     s.setBaud(115200);
     s.flush();
     s.setTimeout(DEFAULT_TIMEOUT);
+
+    //wait for arduino
     std::this_thread::sleep_for(std::chrono::milliseconds(3000));
 
     ::testing::InitGoogleTest(&argc, argv);
